5777df8fe6e0e3344cf37f7dccede2f3df838b13
YuYuanyuan0817 <118308478+YuYuanyuan0817@users.noreply.github.com>
2022-11-19 02:48:23 +0800

    original version

---
+++ hw3-R-yuanyuanyu.R
@@ -0,0 +1,52 @@
+getwd()
+setwd('D:/R????/HW_3')  # set my directory
+df <- read.csv('HW_3/exams.csv', sep=',') #Import dataset to R
+View(df)
+
+#return the dataset structure
+str(df)  
+#return the first five rows of the exam database
+head(df,5) 
+#return the last five rows of the exam database
+tail(df,5)  
+
+#Look at the column names
+colnames(df)  
+# Rename columns which name contains dot
+colnames(df)<-c('gender','race_ethnicity','parental_level_of_education',
+                 'lunch','test_preparation_course','math_score',
+                 'reading_score','writing_score')  
+
+#Change the type of the gender column from character type (chr) to factor type.
+df$gender<-as.character.factor(df$gender)
+#Change the type of the test.preparation.course column from character type (chr) to factor type.
+df$test_preparation_course<-as.character.factor(df$test_preparation_course)
+
+#Add a new column "average_score" to the dataset with the average scores for the three exams "math.score", "reading.score" and "writing.score".
+df$mean<-(df$math_score+df$reading_score+df$writing_score)/3
+colnames(df)[9]<-c('average_score')
+
+#Sort the exam dataframe on 'average_score'column
+df_sorted<-df[order(df$average_score),]
+View(df_sorted)
+
+#Remove the "lunch" column from the exam dataset.
+df[,!names(df)%in%'lunch']
+df[,-grep('lunch',names(df))]
+df[,-4]
+
+#returns subset of students who have completed a test preparation course
+subset(df,subset = test_preparation_course == 'completed')
+
+new_df2<-new_df%>%
+  group_by(age)%>%
+  summarise(mean_n_score=mean(mean_n_score))
+new_df2
+as.numeric(new_df2[,c('age')])
+new_df3<-apply(new_df2,2,as.numeric)
+new_df3<-data.frame(new_df3)
+ggplot(new_df3,aes(x=age))+geom_histogram(fill='lightblue',color='black')
+ggplot(new_df3,aes(x=age,y=mean_n_score))+geom_histogram(fill='lightblue',color='black')
+
+
+
